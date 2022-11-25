4d3545541f2dc5ae4428ab6abcbdda21b4eb6773
Tatiana <tmoseeva@hotmail.com>
2022-11-25 12:01:57 +1100

    add id and key attribute to the list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,16 +3,22 @@ import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
-
+import { nanoid } from 'nanoid';
 import './app.css';
+
+
+function id() {
+	return nanoid();
+}
+
 
 function App() {
 
   const data = [
-    {name: 'John Smith', rate: 800, increase: false},
-    {name: 'Peppa Pig', rate: 900, increase: false},
-    {name: 'Danny Dog', rate: 1100, increase: false},
-    {name: 'Suzy Sheep', rate: 5000, increase: false},
+    {id: id(), name: 'John Smith', rate: 800, increase: false},
+    {id: id(), name: 'Peppa Pig', rate: 900, increase: false},
+    {id: id(), name: 'Danny Dog', rate: 1100, increase: false},
+    {id: id(), name: 'Suzy Sheep', rate: 5000, increase: false},
   ]
 
   return (
