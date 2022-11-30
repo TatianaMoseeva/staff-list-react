34d69975318b7ac8ce1470b15de45ffc42af9cec
Tatiana <tmoseeva@hotmail.com>
2022-12-01 10:10:04 +1100

    add state and onChange event on search input

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,10 +1,17 @@
+import {useState} from 'react';
 import './search-panel.css';
 
 const SearchPanel = () => {
+
+    const [search, setSearch] = useState('');
+
     return (
         <input type="text"
                 className="form-control search-input"
-                placeholder="Find an employee"/>
+                placeholder="Find an employee"
+                value={search}
+                onChange={event => setSearch(event.target.value)}
+        />
     )
 }
 
